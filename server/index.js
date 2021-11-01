d3d38909ad63be36c854525825a4be8101643bd4
buildingsoup <mns0327@naver.com>
2021-11-02 00:07:44 +0900

    feat: add connect mongoDB

--- server/index.js
+++ server/index.js
@@ -1,3 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key');
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(config.mongoURI)
+  .then(() => console.log('MongoDB Connected :)'))
+  .catch((err) => console.log(err));
+
+// support parsing of application/x-www-form-urlencoded post data
+app.use(express.urlencoded({extended: true}));
+
+// support parsing of application/json type post data
+app.use(express.json());
+app.use(cookieParser());
+
+// Router
+// app.use('/api/users', require('./routes/users'));
+
+app.get('/', (req, res) => res.send('hello :)'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`);
+});
