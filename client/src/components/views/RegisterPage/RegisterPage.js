5cdc3bd256a729d0db4d0d860fccd297cc5d395f
buildingsoup <mns0327@naver.com>
2021-11-04 00:47:12 +0900

    feat: update RegisterPage Style

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './RegisterPage.scss';
 
 function RegisterPage() {
+  const [Email, setEmail] = useState('');
+  const [Name, setName] = useState('');
+  const [Password, setPassword] = useState('');
+  const [EmailCheck, setEmailCheck] = useState(false);
+  const [NameCheck, setNameCheck] = useState(false);
+  const [PasswordCheck, setPasswordCheck] = useState(false);
+
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const onNameHandler = (e) => {
+    setName(e.currentTarget.value);
+  };
+
+  const onPasswordHandler = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onEmailCheck = () => {
+    setEmailCheck(true);
+  };
+
+  const onNameCheck = () => {
+    setNameCheck(true);
+  };
+
+  const onPasswordCheck = () => {
+    setPasswordCheck(true);
+  };
+
+  const inputName = (
+    <>
+      <label>사용자 이름을 입력해주세요.</label>
+      <div>
+        <label>→</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+        {NameCheck ? null : (
+          <button type="button" onClick={onNameCheck}>
+            다음
+          </button>
+        )}
+      </div>
+    </>
+  );
+
+  const inputPassword = (
+    <>
+      <label>비밀번호를 입력해주세요.</label>
+      <div>
+        <label>→</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        {PasswordCheck ? null : (
+          <button type="button" onClick={onPasswordCheck}>
+            다음
+          </button>
+        )}
+      </div>
+    </>
+  );
+
+  const register = (
+    <>
+      <button className="registerBtn" onClick>
+        Let's go WATCHED
+      </button>
+    </>
+  );
+
   return (
     <div className="content">
       <h1 className="title">
@@ -13,21 +82,16 @@ function RegisterPage() {
         <label>이메일을 입력해주세요.</label>
         <div>
           <label>→</label>
-          <input type="email" value onChange />
-          <button onClick>다음</button>
+          <input type="email" value={Email} onChange={onEmailHandler} />
+          {EmailCheck ? null : (
+            <button type="button" onClick={onEmailCheck}>
+              다음
+            </button>
+          )}
         </div>
-        <label>사용자 이름을 입력해주세요.</label>
-        <div>
-          <label>→</label>
-          <input type="text" value onChange />
-          <button onClick>다음</button>
-        </div>
-        <label>비밀번호를 입력해주세요.</label>
-        <div>
-          <label>→</label>
-          <input type="password" value onChange />
-          <button onClick>다음</button>
-        </div>
+        {EmailCheck ? inputName : null}
+        {NameCheck ? inputPassword : null}
+        {PasswordCheck ? register : null}
       </form>
     </div>
   );
