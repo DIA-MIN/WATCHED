c5966fe769ffbe1591a47a95312d6d0ac7a8cd12
buildingsoup <mns0327@naver.com>
2021-11-03 23:39:32 +0900

    feat: add RegisterPage Style

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -4,9 +4,31 @@ import './RegisterPage.scss';
 function RegisterPage() {
   return (
     <div className="content">
-      <h1 className="title">WATCHED에 가입하고 영화를 평가해 보세요.</h1>
+      <h1 className="title">
+        <span>WATCHED</span>에 가입하고 영화를 평가해 보세요.
+      </h1>
       <br />
       <p className="subtitle">Welcome to WATCHED!</p>
+      <form onSubmit>
+        <label>이메일을 입력해주세요.</label>
+        <div>
+          <label>→</label>
+          <input type="email" value onChange />
+          <button onClick>다음</button>
+        </div>
+        <label>사용자 이름을 입력해주세요.</label>
+        <div>
+          <label>→</label>
+          <input type="text" value onChange />
+          <button onClick>다음</button>
+        </div>
+        <label>비밀번호를 입력해주세요.</label>
+        <div>
+          <label>→</label>
+          <input type="password" value onChange />
+          <button onClick>다음</button>
+        </div>
+      </form>
     </div>
   );
 }
