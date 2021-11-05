acfa160ee653d54cf04cee4d707c088675afeadb
buildingsoup <mns0327@naver.com>
2021-11-06 00:42:05 +0900

    feat: update NavBar Style

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,22 +1,41 @@
 import React, {useState} from 'react';
-import {FaSearch} from 'react-icons/fa';
+import {FaSearch, FaBars, FaClose, FaTimes} from 'react-icons/fa';
 import './NavBar.scss';
 
 function NavBar() {
+  const [Clicked, setClicked] = useState(false);
+
+  const onClickHandle = () => {
+    setClicked(!Clicked);
+  };
+
   return (
     <nav className="navContainer">
       <div className="logo">
-        <span>WATCHED</span>
+        <a href="/">WATCHED</a>
       </div>
-      <form className="search">
-        <input />
-        <FaSearch className="search-icon" />
-      </form>
-      <span></span>
-      <ul className="nav-menu">
-        <li>로그인</li>
-        <li>회원가입</li>
+
+      <ul className={Clicked ? 'nav-menu active' : 'nav-menu'}>
+        <li>
+          <form className="search">
+            <input />
+            <FaSearch className="search-icon" />
+          </form>
+        </li>
+        <a href="/login">
+          <li>로그인</li>
+        </a>
+        <a href="/register">
+          <li>회원가입</li>
+        </a>
       </ul>
+      <div className="menu" onClick={onClickHandle}>
+        {Clicked ? (
+          <FaTimes className="toggle-icon" />
+        ) : (
+          <FaBars className="toggle-icon" />
+        )}
+      </div>
     </nav>
   );
 }
